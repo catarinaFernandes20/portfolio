7fe051a00d670c9c8734e1c9198665a0a9030f03
Catarina Natacha <75850059+catarinaFernandes20@users.noreply.github.com>
2021-06-24 08:08:52 +0100

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
   {
     path:"Navbar", component:NavbarComponent
   },
-  {path:"NotFound", component:ErroComponent},
+  {path:"Erro", component:ErroComponent},
   {path:"Chuck Norris", component:ChucknorrisComponent},
   {path:"Lista De Compras", component:ListaDeComprasComponent,
     children:
